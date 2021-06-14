ea81653afe2a62a3874985fd4240551b3e98115a
Obioma Osuagwu <justobioma@outlook.com>
2021-06-14 16:15:09 +0100

    Committing the first set of files to local repo - Able to see my video

---
+++ plan.md
@@ -0,0 +1,11 @@
+Initialise our Nodejs project
+Initialise our first view
+Create a room id
+Add the ability to view our own video
+Add the ability to allow others to stream their video
+Add styling
+Add the ability to create messages
+Add mute button
+Add Stop Video button
+
+Nodejs, UUid, Socket.IO, Express server
---
+++ public/script.js
@@ -0,0 +1,64 @@
+//Where all the js  for the frontend will live
+
+//to create the functionality see our own video
+
+const socket = io("/");
+const videoGrid = document.getElementById("video-grid");
+//console.log(videoGrid);
+const myVideo = document.createElement("video");
+myVideo.muted = true;
+
+var peer = new Peer(undefined, {
+  path: "/peerjs",
+  host: "/",
+  port: 3030,
+});
+
+//let myVideoStream;
+//const ROOM_ID;
+
+navigator.mediaDevices
+  .getUserMedia({
+    video: true,
+    audio: true,
+  })
+  .then((stream) => {
+    //myVideoStream = stream;
+    addVideoStream(myVideo, stream);
+
+    peer.on("call", (call) => {
+      call.answer(stream);
+      const video = document.createElement("video");
+      call.on("stream", (userVideoStream) => {
+        addVideoStream(video, userVideoStream);
+      });
+    });
+
+    socket.on("user-connected", (userId) => {
+      connecToNewUser(userId, stream);
+    });
+  });
+
+peer.on("open", (id) => {
+  socket.emit("join-room", ROOM_ID, id);
+});
+
+const connecToNewUser = (userId, stream) => {
+  const call = peer.call(userId, stream);
+  const video = document.createElement("video");
+  call.on("stream", (userVideoStream) => {
+    addVideoStream(video, userVideoStream);
+  });
+
+  call.on("close", () => {
+    video.remove();
+  });
+};
+
+const addVideoStream = (video, stream) => {
+  video.srcObject = stream;
+  video.addEventListener("loadedmetadata", () => {
+    video.play();
+  });
+  videoGrid.append(video);
+};
